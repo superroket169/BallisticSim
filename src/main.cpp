c1c978f8435a9ce09908c1396dc542d76ea9cc73
superroket169 <isagullu2010@gmail.com>
2025-12-17 21:30:59 +0300

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 
+const double gravityAccel = 10.0;
+
+double calc(double velX, double velY)
+{
+    double length = -1;
+
+    double flyTime = velY / 10.0;
+    length = flyTime * velX;
+
+    return length;
+}
+
 signed main()
 {
+    std::cout << "[Input]: Enter velocity(X) and velocity(y):\n";
+    double i, j;
+    std::cin >> i >> j;
 
+    std::cout << calc(i, j) << "\n";
     
     return 0;
 }
