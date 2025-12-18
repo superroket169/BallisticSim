085c36980d36337c1f1ca808d357a69003d4b10b
superroket169 <isagullu2010@gmail.com>
2025-12-18 11:39:03 +0300

    basic mecks

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,84 @@
 #include <iostream>
+#include <cmath>
+#include <SFML/Graphics.hpp>
 
-const double gravityAccel = 10.0;
+const double gravityAccel = 0.0001;
+
+struct velocity
+{
+    double x;
+    double y;
+};
+
 
 double calc(double velX, double velY)
 {
     double length = -1;
 
-    double flyTime = velY / 10.0;
-    length = flyTime * velX;
+    double flightTime = velY / 10.0;
+    length = flightTime * velX;
 
     return length;
 }
 
 signed main()
 {
-    std::cout << "[Input]: Enter velocity(X) and velocity(y):\n";
-    double i, j;
-    std::cin >> i >> j;
+    const int windowX = 1200;
+    const int windowY = 800;
+    sf::RenderWindow window(sf::VideoMode(windowX, windowY), "PixelDrift");
 
-    std::cout << calc(i, j) << "\n";
+    sf::CircleShape mass(20);
+    mass.setFillColor(sf::Color::Black);
+    mass.setOrigin(mass.getRadius(), mass.getRadius());
+    mass.setPosition(100, 680);
+
+    sf::RectangleShape floor({1200, 100});
+    floor.setFillColor(sf::Color::Red);
+    floor.setPosition(0, 0);
+    floor.setPosition(0, windowY - 100);
+
+    double speed = 0;
+    double velocityX = 0;
+    double velocityY = 0;
+
+    sf::Clock clock;
+
+    velocity velocityInput = {0.2, -0.2};
+    //std::cin >> velocityInput.x >> velocityInput.y;
+    velocityX = velocityInput.x;
+    velocityY = velocityInput.y;
     
+    while(window.isOpen())
+    {
+        float dt = clock.getElapsedTime().asSeconds();
+        window.clear(sf::Color(200, 255, 200));
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed || event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) window.close();
+        }    
+
+        speed = std::sqrt((velocityX * velocityX) + (velocityY * velocityY));
+
+        {// acceleration:
+            if(mass.getPosition().y < 700)
+            {
+                velocityY += gravityAccel * dt;
+            }
+            else
+            {
+                velocityX = 0;
+                velocityY = 0;
+            }
+            mass.move(velocityX * dt, velocityY * dt);
+        }
+
+        window.draw(floor);
+        window.draw(mass);
+        window.display();
+
+        std::cout << int(velocityY * 100) << "\n";
+    }
+
     return 0;
 }
